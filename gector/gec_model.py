b4a5c768f259d2934314a28ea6085f1bff0ddbc1
ruohai0925 <ruohai372@gmail.com>
2021-02-22 12:45:41 -0600

    Modify the gector to build the API between gector and chatterbox

--- gector/gec_model.py
+++ gector/gec_model.py
@@ -16,7 +16,7 @@ from allennlp.nn import util
 from gector.bert_token_embedder import PretrainedBertEmbedder
 from gector.seq2labels_model import Seq2Labels
 from gector.wordpiece_indexer import PretrainedBertIndexer
-from utils.helpers import PAD, UNK, get_target_sent_by_edits, START_TOKEN
+from gector.utils.helpers import PAD, UNK, get_target_sent_by_edits, START_TOKEN
 
 logging.getLogger("werkzeug").setLevel(logging.ERROR)
 logger = logging.getLogger(__file__)
